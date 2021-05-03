b9a6338b534b7e3eec319bb13f7c424d4cc29793
Pawan bisht <48342428+Pawan300@users.noreply.github.com>
2021-05-03 10:43:02 +0530

    Add files via upload

---
+++ Integrated_web_cam/README.md
@@ -0,0 +1,17 @@
+# Integrate_webcam_Python
+
+This will help you integrate your webcam with the program.
+
+Requirement:
+- opencv
+- python3
+
+How to run:
+- open terminal
+- python camera_use.py
+
+If you want to store your output:
+- open terminal
+- python webcam.py --output_path "/Videos/recording.avi"
+
+Thanks and enjoy coding
---
+++ Integrated_web_cam/camera_use.py
@@ -0,0 +1,37 @@
+import cv2
+
+
+class Live_video:
+
+    def __init__(self):
+        self.frame = None
+        self.check = None
+        self.video = cv2.VideoCapture(0)
+
+    def record(self):
+        self.check, self.frame = self.video.read()
+
+        return self.frame
+
+    def release(self):
+        self.video.release()
+        cv2.destroyAllWindows()
+
+
+def main():
+    l = Live_video()
+    while True:
+        frame = l.record()
+        print(frame)
+        cv2.namedWindow("Live_video", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("Live_video", 600, 600)
+        frame = cv2.putText(frame, "Press q to quit...", (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.imshow("Live_video", frame)
+        if cv2.waitKey(1) == ord("q"):
+            break
+
+    l.release()
+
+
+if __name__ == '__main__':
+    main()
